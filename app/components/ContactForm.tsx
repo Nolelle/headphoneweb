04462fea6deb054c1fc7aedfbefd276e60ac3bbf
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2024-11-28 00:35:28 -0700

    change contact form message

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -40,8 +40,10 @@ const ContactForm: React.FC = () => {
           Contact Us
         </h2>
         <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature?
-          Need details about our Business plan? Let us know.
+          Have questions about our products? Whether you're seeking technical
+          details, exploring features, or interested in learning more about our
+          solutions, we're here to help. Reach out to our dedicated team for
+          personalized assistance.
         </p>
         <form
           action="#"
@@ -59,7 +61,7 @@ const ContactForm: React.FC = () => {
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="email@email.com"
               required
             />
           </div>
